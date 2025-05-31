4b37ef2d1d5778800b599aef3ff3a568e949b8bc
duyphan119 <duy.910.2015@gmail.com>
2025-05-31 13:27:33 +0700

    fix no cache

--- next.config.ts
+++ next.config.ts
@@ -13,10 +13,9 @@ const nextConfig: NextConfig = {
     {
       source: "/:path*",
       headers: [
-        {
-          key: "Cache-Control",
-          value: "no-store",
-        },
+         { key: 'Cache-Control', value: 'no-store, no-cache, must-revalidate, proxy-revalidate' },
+                    { key: 'Pragma', value: 'no-cache' },
+                    { key: 'Expires', value: '0' },
       ],
     },
   ],
